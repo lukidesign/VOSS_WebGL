eb7f55692f749a3527027461c16d14fdd5341e94
lukidesign <32728439+lukidesign@users.noreply.github.com>
2026-07-09 12:28:11 +0800

    Update build: VOSS_WebGL_2 (0709 版本)
    - Build 文件重命名 VOSS_WebGL.* -> VOSS_WebGL_2.*
    - 更新 index.html / ServiceWorker.js 引用新文件名
    - 内容更新（Unity 重新构建产物）

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-VOSS_PowerFactory_2022URP_WebGL_1920_1080-0.1.0";
 const contentToCache = [
-    "Build/VOSS_WebGL.loader.js",
-    "Build/VOSS_WebGL.framework.js.unityweb",
-    "Build/VOSS_WebGL.data.unityweb",
-    "Build/VOSS_WebGL.wasm.unityweb",
+    "Build/VOSS_WebGL_2.loader.js",
+    "Build/VOSS_WebGL_2.framework.js.unityweb",
+    "Build/VOSS_WebGL_2.data.unityweb",
+    "Build/VOSS_WebGL_2.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
